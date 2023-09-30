5b3be61603194cc649856274760b72504c347084
qww <497363953@qq.com>
2023-09-30 18:47:38 +0800

    feat(composables): useSelection

---
+++ src/composables/useSelection/index.ts
@@ -0,0 +1,43 @@
+import { bitable, Selection } from "@lark-base-open/js-sdk"
+import { ref, onUnmounted, onMounted } from "vue"
+import { onSelectionChange } from ".."
+
+interface useSelectionOptions {
+  onChanged?: (selection: Selection) => void
+}
+
+/**
+ * Reactive bitable selection
+ *
+ * @param options
+ */
+export function useSelection(options: useSelectionOptions = {}) {
+  const baseId = ref<string | null>(null)
+  const recordId = ref<string | null>(null)
+  const fieldId = ref<string | null>(null)
+  const viewId = ref<string | null>(null)
+  const tableId = ref<string | null>(null)
+  const { onChanged } = options
+  onMounted(() => {
+    bitable.base.getSelection().then((e) => {
+      baseId.value = e.baseId
+      recordId.value = e.recordId
+      fieldId.value = e.fieldId
+      viewId.value = e.viewId
+      tableId.value = e.tableId
+    })
+  })
+
+  const unListenSelection = onSelectionChange(async (e) => {
+    baseId.value = e.baseId
+    recordId.value = e.recordId
+    fieldId.value = e.fieldId
+    viewId.value = e.viewId
+    tableId.value = e.tableId
+    onChanged && onChanged(e)
+  })
+
+  onUnmounted(() => {
+    unListenSelection()
+  })
+}
