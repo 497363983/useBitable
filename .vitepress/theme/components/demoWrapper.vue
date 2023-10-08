1bf56a69360d0f02e53ed45620b46624ae38c6a2
qww <497363953@qq.com>
2023-10-08 20:32:05 +0800

    docs: fix ssr

--- .vitepress/theme/components/demoWrapper.vue
+++ .vitepress/theme/components/demoWrapper.vue
@@ -1,35 +1,27 @@
 <script setup lang="ts">
-import { onErrorCaptured, ref } from "vue"
-import { useEnv } from "@qww0302/use-bitable"
-import { useData } from "vitepress"
-
-const { lang } = useData()
+import { onErrorCaptured, ref, defineAsyncComponent } from "vue"
 
 defineProps<{
   src: string;
 }>();
 
-const env = useEnv();
-
 const error = ref<Error | null>(null);
 
-const notice: Record<string, string> = {
-  zh: "请在多维表格中打开本文档查看完整效果",
-  en: "Please open in Bitable to see the full effect",
-}
 
 onErrorCaptured((err) => {
   error.value = err;
 });
+
+// @ts-ignore
+const envNotice = defineAsyncComponent(() => import("./envNotice.vue"));
 </script>
 
 <template>
-  <div v-if="!env">
-    <div class="warning custom-block">
-      <p class="custom-block-title">WARNING</p>
-      <p>{{ notice[Object.keys(notice).includes(lang) ? lang : "en"] }}</p>
-    </div>
-  </div>
+  <ClientOnly>
+    <Suspense>
+      <envNotice />
+    </Suspense>
+  </ClientOnly>
   <div class="demo">
     <p>
       <a class="demo-source-link" :href="src">Source</a>
---
+++ .vitepress/theme/components/envNotice.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useEnv } from "@qww0302/use-bitable"
+import { useData } from "vitepress"
+
+const { lang } = useData()
+const env = useEnv()
+
+const notice: Record<string, string> = {
+  zh: "请在多维表格中打开本文档查看完整效果",
+  en: "Please open in Bitable to see the full effect",
+}
+</script>
+
+<template>
+  <div v-if="!env">
+    <div class="warning custom-block">
+      <p class="custom-block-title">WARNING</p>
+      <p>{{ notice[Object.keys(notice).includes(lang) ? lang : "en"] }}</p>
+    </div>
+  </div>
+</template>
