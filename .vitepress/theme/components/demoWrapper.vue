04b90669c1da44f991ee888579ce67c7ff82dfb9
qww <497363953@qq.com>
2023-10-08 19:28:06 +0800

    docs: warning for env

--- .vitepress/theme/components/demoWrapper.vue
+++ .vitepress/theme/components/demoWrapper.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { onErrorCaptured, ref } from "vue";
+import { onErrorCaptured, ref } from "vue"
+import { useEnv } from "@qww0302/use-bitable"
+import { useData } from "vitepress"
+
+const { lang } = useData()
 
 defineProps<{
   src: string;
 }>();
 
+const env = useEnv();
+
 const error = ref<Error | null>(null);
+
+const notice: Record<string, string> = {
+  zh: "请在多维表格中打开本文档查看完整效果",
+  en: "Please open in Bitable to see the full effect",
+}
 
 onErrorCaptured((err) => {
   error.value = err;
@@ -13,6 +24,12 @@ onErrorCaptured((err) => {
 </script>
 
 <template>
+  <div v-if="!env">
+    <div class="warning custom-block">
+      <p class="custom-block-title">WARNING</p>
+      <p>{{ notice[Object.keys(notice).includes(lang) ? lang : "en"] }}</p>
+    </div>
+  </div>
   <div class="demo">
     <p>
       <a class="demo-source-link" :href="src">Source</a>
@@ -24,6 +41,4 @@ onErrorCaptured((err) => {
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
