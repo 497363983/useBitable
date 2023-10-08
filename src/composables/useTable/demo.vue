06ffc4344caef05f01536e1da89773d7ebe93b72
qww <497363953@qq.com>
2023-10-08 22:28:22 +0800

    docs(composables): useTable

--- src/composables/useTable/demo.vue
+++ src/composables/useTable/demo.vue
@@ -3,15 +3,15 @@ import { useTable, useSelection } from "@qww0302/use-bitable"
 import { ref, watch } from "vue"
 
 const { tableId } = useSelection()
-const table = useTable(tableId)
-const name = ref("")
+const { table, pending } = useTable(tableId)
+const name = ref()
 
 watch(
   () => table.value,
-  (t)=>{
-    name.value = ""
-    if(t) {
-      t.getName().then(res=>{
+  (t) => {
+    name.value = null
+    if (t) {
+      t.getName().then(res => {
         name.value = res
       })
     }
@@ -21,9 +21,15 @@ watch(
 </script>
 
 <template>
+  <div class="tip custom-block">
+    <p class="custom-block-title">
+      TIP
+    </p>
+    <p>Choose different table</p>
+  </div>
   <div>
-    Table name: 
-    <span v-if="name">{{ name }}</span>
-    <span v-else>loading table name...</span>
+    Table name:
+    <span v-if="!pending && name">{{ name ?? "null" }}</span>
+    <span v-else-if="pending || !name">loading table...</span>
   </div>
 </template>
--- src/composables/useTable/index.zh.md
+++ src/composables/useTable/index.zh.md
@@ -1,3 +1,11 @@
 # useTable
 
 ## 用法
+
+`useTable` 返回的 `table` 为响应式的 `ITable` 对象，实时根据传入的参数变化。传入的值可以为 `table` 的 `id` 或者 `name`。
+
+::: tip
+该函数是基于 [`getTable`](https://lark-base-team.github.io/js-sdk-docs/zh/api/base#gettable) 实现的，由于官方的 `API` 均为 `异步函数`，所以 `table` 的更新会有一定延迟。因此，该函数提供了 `pending` 来反映获取状态，为 `true` 时则正在获取 `table`。
+:::
+
+<<< ./_public/usage.ts
