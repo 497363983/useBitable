bd28a9b9c01f39017a232ca6b18da25b8e11b633
qww <497363953@qq.com>
2023-11-24 09:20:01 +0800

    feat(events): onRecordAdd

---
+++ src/events/onRecordAdd/demo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onRecordAdd, useSelection } from "@qww0302/use-bitable"
+import type { IEventCbCtx } from "@lark-base-open/js-sdk"
+import { ref } from "vue"
+
+const { tableId } = useSelection()
+const ev = ref<IEventCbCtx<[recordId: string]>>()
+onRecordAdd(tableId, (e) => {
+  ev.value = e
+})
+</script>
+
+<template>
+  <div>
+    Try to add records:
+    <span v-if="ev">You have added {{ ev.data.length }} record{{ ev.data.length > 1 ? "s" : "" }}. </span>
+    <pre>{{ JSON.stringify(ev, null, 2) }}</pre>
+  </div>
+</template>
---
+++ src/events/onRecordAdd/index.md
@@ -0,0 +1,3 @@
+# onRecordAdd
+
+## Usage
