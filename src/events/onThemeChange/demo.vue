53ba951d101369bde82388d6614d8e8bb4d86dc1
qww <497363953@qq.com>
2023-11-24 21:01:11 +0800

    docs: typo

--- src/events/onThemeChange/demo.vue
+++ src/events/onThemeChange/demo.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { onThemeChange } from "@qww0302/use-bitable"
+import { bitable } from "@lark-base-open/js-sdk"
+import { ref, onMounted } from "vue"
 
+const theme = ref("LIGHT")
+
+onThemeChange((t) => {
+  theme.value = t
+})
+
+onMounted(()=>{
+  bitable.bridge.getTheme().then((t) => {
+    theme.value = t
+  })
+})
 </script>
 
 <template>
   <div>
-    <h1>Template</h1>
+    Current theme: {{ theme }}
   </div>
 </template>
