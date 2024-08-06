af030e29d5452930b3c729ee55d973ae36f05d29
qww <497363953@qq.com>
2024-08-06 17:16:14 +0800

    docs: theme fix

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,15 @@
 // https://vitepress.dev/guide/custom-theme
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style/vars.css'
 import './style/demo.css'
 import './style/footer.css'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
-}
+} satisfies Theme
