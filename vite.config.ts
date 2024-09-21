0e2ed54278610de3242ad325237bc95e59cc167a
qww <497363953@qq.com>
2024-09-21 22:54:23 +0800

    chore: fix test

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     include: ["test/**/*.{test,spec}.?(c|m)[jt]s?(x)", "src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
-    exclude: ["node_modules", "_*/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    exclude: ["node_modules", "**/_*/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
     reporters: ["html", "default"],
     outputFile: path.resolve("./public/dist/test/index.html"),
     alias: {
